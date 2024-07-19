96186088ad00b2dfe9bff8809f5945dfd81329d9
SJKIM\ksj10 <ksj10111011@naver.com>
2024-07-19 15:35:49 +0900

    feat : make spi speed be slow, tdc7200 init code 수정

--- 1_stm32_h750_repo/src/hw/driver/tdc7200.c
+++ 1_stm32_h750_repo/src/hw/driver/tdc7200.c
@@ -32,16 +32,27 @@ void tdc7200Init(void)
 	int stop_index = ENUM_TDC7200_CLOCK_CNTR_STOP_MASK_L;
 	for(int i = start_index; i <= stop_index; i++)
 	{
+		uint8_t tx_buff[2] = {start_index, };
+		uint8_t rx_buff[2] = {0, };
 		HAL_GPIO_WritePin(CSB_GPIO_Port, CSB_Pin, GPIO_PIN_RESET);
-		HAL_SPI_Receive(&hspi2, &tdc7200_inst.reg_1_byte_data[i], 1, 0xFFFF);
+		HAL_SPI_TransmitReceive(&hspi2, tx_buff, rx_buff, 2, 0xFFFF);
 		HAL_GPIO_WritePin(CSB_GPIO_Port, CSB_Pin, GPIO_PIN_SET);
+
+		tdc7200_inst.reg_1_byte_data[i] = rx_buff[1];
 	}
 	start_index = ENUM_TDC7200_TIME_1 - ENUM_TDC7200_TIME_1;
 	stop_index = ENUM_TDC7200_CALIBRATION_2 - ENUM_TDC7200_TIME_1;
 	for(int i = start_index; i <= stop_index; i++)
 	{
+		uint8_t tx_buff[5] = {start_index, };
+		uint8_t rx_buff[5] = {0, };
+
 		HAL_GPIO_WritePin(CSB_GPIO_Port, CSB_Pin, GPIO_PIN_RESET);
-		HAL_SPI_Receive(&hspi2, tdc7200_inst.reg_3_byte_data[i], 3, 0xFFFF);
+		HAL_SPI_TransmitReceive(&hspi2, tx_buff, rx_buff, 5, 0xFFFF);
 		HAL_GPIO_WritePin(CSB_GPIO_Port, CSB_Pin, GPIO_PIN_SET);
+
+		tdc7200_inst.reg_3_byte_data[i][0] = rx_buff[3];
+		tdc7200_inst.reg_3_byte_data[i][1] = rx_buff[2];
+		tdc7200_inst.reg_3_byte_data[i][2] = rx_buff[1];
 	}
 }
